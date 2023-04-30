7650958ce125bb933eff375011b89aed1192ba7f
jacksonPeaku <jackson.avila@peaku.ai>
2023-04-30 11:36:37 -0500

    delet run proyect cmd

--- run-proyect.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe --dbpath C:\data\db"
-start cmd.exe /k "cd C:\Users\elecllanos\Documents\jack_nodeJs\React Proyect\RedSocial\api-rest-red-social && npm start" 
